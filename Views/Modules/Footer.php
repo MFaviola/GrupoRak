aad2a51e53232d442846752df7ebf2fa9fa1771d
Madian2112 <109115569+FernanJS17@users.noreply.github.com>
2024-06-13 15:34:39 -0600

    comienzo de cambios de diseño

--- Views/Modules/Footer.php
+++ Views/Modules/Footer.php
@@ -1,4 +1,4 @@
-<footer class="main-footer">
+<footer class="main-footer" style="color:#9c9c9c; background-color:#000">
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
--- Views/Modules/Menu.php
+++ Views/Modules/Menu.php
@@ -1,10 +1,10 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-image: url(https://i.pinimg.com/736x/95/62/3d/95623dc132235138cfa75403d63aeef3.jpg);">
     <!-- Brand Logo -->
-    <a href="Views/Resources/index3.html" class="brand-link">
-      <img src="Views/Resources/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="Views/Resources/index3.html" class="brand-link" style="background-color:#000">
+      <img src="Views\Resources\dist\img\logroRac.jpg" alt="grupo rac" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Grupo Rac</span>
     </a>
-
+    
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user (optional) -->
@@ -13,7 +13,7 @@
           <img src="Views/Resources/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Usuario invitado</a>
         </div>
       </div>
 
