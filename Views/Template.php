e6d9f2880193ffb9faa348f6f1edc8dea291786f
Madian2112 <109115569+FernanJS17@users.noreply.github.com>
2024-06-14 11:03:20 -0600

    +diseño

--- Views/Template.php
+++ Views/Template.php
@@ -4,18 +4,18 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Blank Page</title>
+  <title>Grupo Rac</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="Views/Resources/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="Views/Resources/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="Views/Resources/dist/css/adminlte.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
     <!-- DataTables -->
-    <link rel="stylesheet" href="Views/Resources/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="Views/Resources/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
   <link rel="stylesheet" href="Views/Resources/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="Views/Resources/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 </head>
@@ -69,7 +69,7 @@
 <script src="Views/Resources/dist/js/demo.js"></script>
 <!-- DataTables  & Plugins -->
 <script src="Views/Resources/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="Views/Resources/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="Views/Resources/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
 <script src="Views/Resources/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
 <script src="Views/Resources/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
 <script src="Views/Resources/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
